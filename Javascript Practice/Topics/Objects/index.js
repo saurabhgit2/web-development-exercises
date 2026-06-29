bccbc0f3a75d7b29cce8be8770461bfbd78e566e
saurabhgit2 <saurabhgithub0@gmail.com>
2026-06-29 22:15:35 +1200

    Revising JS Objects

---
+++ Javascript Practice/Topics/Objects/index.js
@@ -0,0 +1,24 @@
+// Create Object
+
+// let rectangle={
+//     length:1,
+//     breadth:2,
+//     draw: function(){
+//        console.log("This is object's method.");
+//     }
+// };
+
+
+//Factory function to create 
+
+function createRectangle () {
+    return rectangle={
+    length:1,
+    breadth:2,
+    draw: function(){
+       console.log("This is object's method.");
+    }
+};
+}
+
+let rectangle1=createRectangle()
