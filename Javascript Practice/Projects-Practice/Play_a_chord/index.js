7c416b367c082e32983c7a793a302367257bc152
saurabhgit2 <saurabhgithub0@gmail.com>
2026-07-02 16:08:35 +1200

    Added button to play E minor chord

--- Javascript Practice/Projects-Practice/Play_a_chord/index.js
+++ Javascript Practice/Projects-Practice/Play_a_chord/index.js
@@ -1,5 +1,8 @@
 const playCchord=document.getElementById("btnforC");
 const playDchord=document.getElementById("btnforD");
+const btnforEm=document.getElementById("btnforEm");
+
+const emChord=document.getElementById("emChord");
 const cChord =document.getElementById("cChord");
 const dChord =document.getElementById("dChord");
 const strumDC=document.getElementById("strumDC");
@@ -7,7 +10,7 @@ function playboth(){
     const cChord =document.getElementById("cChord");
     const dChord =document.getElementById("dChord");
     cChord.play();
-    cChord.addEventListener('ended', function(){dChord.play()});
+    cChord.addEventListener('ended', function(){dChord.play();});
 };
 function playChord(){
     cChord.play();
@@ -15,7 +18,12 @@ function playChord(){
 function play_chord(){
     dChord.play();
 };
+
+function playEminor(){
+    emChord.play();
+}
 playCchord.addEventListener('click',playChord);
 playDchord.addEventListener('click',play_chord);
 
 strumDC.addEventListener('click',playboth);
+btnforEm.addEventListener('click',playEminor);
