ee1e4b54030b1e9f9fe439335fe69f7b8f9c63eb
saurabhgit2 <saurabhgithub0@gmail.com>
2026-07-08 21:04:07 +1200

    Getter_setter and try catch error practiced

---
+++ Javascript Practice/Topics/Try_Catch_error/index.js
@@ -0,0 +1,12 @@
+// Try and catch error
+let x=0;
+// let y=1;
+try{
+   console.log("Testing");
+    let z=x+y; 
+    console.log("Tested");
+}
+
+catch(e){
+    console.log(` Error Code-${e} \n Something went wrong, recheck the code`);
+}
