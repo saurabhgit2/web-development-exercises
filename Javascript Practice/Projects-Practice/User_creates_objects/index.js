c528ef77bd0180624cba1a13c2ac77da73294751
saurabhgit2 <saurabhgithub0@gmail.com>
2026-07-09 13:25:42 +1200

    user created array of objects

--- Javascript Practice/Projects-Practice/User_creates_objects/index.js
+++ Javascript Practice/Projects-Practice/User_creates_objects/index.js
@@ -1,11 +1,17 @@
 const submit=document.getElementById("Submit");
 const person={};
+const createdObj=document.getElementById("createdObj");
+
 
 function addData(){
       const key=document.getElementById("key").value;
       const value=document.getElementById("value").value;
       person[key]=value;
       console.log(person);
+      
+      createdObj.textContent=JSON.stringify(person, null, 2);
 };
 
 submit.addEventListener('click',addData);
+
+
